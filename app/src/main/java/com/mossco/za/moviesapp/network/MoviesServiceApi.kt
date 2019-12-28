298b3c7b423fb1a27257e4db3ecf9a9cc38d30b9
moses14 <kgoputsom@discovery.co.za>
2019-12-28 23:05:20 +0200

    Rename method name

--- app/src/main/java/com/mossco/za/moviesapp/network/MoviesServiceApi.kt
+++ app/src/main/java/com/mossco/za/moviesapp/network/MoviesServiceApi.kt
@@ -5,5 +5,5 @@ import retrofit2.http.Query
 
 interface MoviesServiceApi {
     @GET("now_playing?")
-    fun getCurrentlyPlayingMoviesAsync(@Query("api_key") apiKey: String): NetworkMoviesResponse
+    fun getCurrentlyPlayingMovies(@Query("api_key") apiKey: String): NetworkMoviesResponse
 }
